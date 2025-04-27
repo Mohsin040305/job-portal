5071970b253d7e64c51cf4585a2462e3752b1ee5
Mohsin040305 <mistrymohsin4@gmail.com>
2025-04-27 11:50:47 +0530

    added optional chaning in applicants table

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -12,12 +12,12 @@ const shortListingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
     const { applicants } = useSelector(store => store.application);
 
-    const statusHandler = async (status,id) => {
+    const statusHandler = async (status, id) => {
         try {
-            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, {status},{
-                withCredentials:true
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, { status }, {
+                withCredentials: true
             });
-            if(res.data.success){
+            if (res.data.success) {
                 toast.success(res.data.message);
             }
         } catch (error) {
@@ -43,15 +43,15 @@ const ApplicantsTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        applicants && applicants.applications.map((item) => (
+                        applicants && applicants?.applications?.map((item) => (
                             <TableRow key={item._id}>
                                 <TableCell>{item?.applicant?.fullname}</TableCell>
                                 <TableCell>{item?.applicant?.email}</TableCell>
                                 <TableCell>{item?.applicant?.phoneNumber}</TableCell>
                                 <TableCell>
                                     {item?.applicant?.profile?.resume ?
-                                    <a className='text-blue-600 cursor-pointer' href={item?.applicant?.profile?.resume} target="_blank">{item.applicant.profile.resumeOrignalName}
-                                    </a> : <span>NA</span>
+                                        <a className='text-blue-600 cursor-pointer' href={item?.applicant?.profile?.resume} target="_blank">{item.applicant.profile.resumeOrignalName}
+                                        </a> : <span>NA</span>
                                     }</TableCell>
                                 <TableCell>{item?.applicant?.createdAt.split("T")[0]}</TableCell>
                                 <TableCell className="float-right cursor-pointer">
@@ -63,7 +63,7 @@ const ApplicantsTable = () => {
                                             {
                                                 shortListingStatus?.map((status, index) => {
                                                     return (
-                                                        <div onClick={() => statusHandler(status,item._id)} key={index} className='flex w-fit items-center ny-2 cursor-pointer'>
+                                                        <div onClick={() => statusHandler(status, item._id)} key={index} className='flex w-fit items-center ny-2 cursor-pointer'>
                                                             <span>{status}</span>
                                                         </div>
                                                     )
