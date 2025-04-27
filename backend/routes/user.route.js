92bba0b1f72535a42a1b00905f67662233e2a8e1
Mohsin040305 <mistrymohsin4@gmail.com>
2025-04-27 12:23:58 +0530

    added single upload in user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { login,logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
-import { multipleUpload } from "../middleware/multer.js";
+import { multipleUpload,singleUpload } from "../middleware/multer.js";
 
 const router = express.Router();
 
