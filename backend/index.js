625692bfb18fe46f7a67740cac74193fe0e62802
Mohsin040305 <mistrymohsin4@gmail.com>
2026-02-04 22:35:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-frontend-kd0g.onrender.com/',
+    origin:'https://job-portal-frontend-kd0g.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
